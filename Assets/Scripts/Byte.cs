963514712b40ef3d79190ff893291b01cf25114e
D'onell/F4RWAY <donfrank2007@gmail.com>
2025-03-06 14:42:26 +0100

    update

--- Assets/Scripts/Byte.cs
+++ Assets/Scripts/Byte.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks.Sources;
 using UnityEngine;
 
 public class Byte : MonoBehaviour
@@ -26,4 +28,105 @@ public class Byte : MonoBehaviour
         if (bits[6].state) { value += 64; }
         if (bits[7].state) { value += 128; }
     }
+
+    public void setValue(int myValue)
+    {
+        if (myValue > 255)
+        {
+            myValue = 255;
+        }
+
+        this.value = myValue;
+
+        if(myValue >= 128)
+        {
+            bits[7].state = true;
+
+            myValue -= 128;
+        }
+        else
+        {
+            bits[7].state = false;
+        }
+
+        if (myValue >= 64)
+        {
+            bits[6].state = true;
+
+            myValue -= 64;
+        }
+        else
+        {
+            bits[6].state = false;
+        }
+
+        if (myValue >= 32)
+        {
+            bits[5].state = true;
+
+            myValue -= 32;
+        }
+        else
+        {
+            bits[5].state = false;
+        }
+
+        if (myValue >= 16)
+        {
+            bits[4].state = true;
+
+            myValue -= 16;
+        }
+        else
+        {
+            bits[4].state = false;
+        }
+
+        if (myValue >= 8)
+        {
+            bits[3].state = true;
+
+            myValue -= 8;
+        }
+        else
+        {
+            bits[3].state = false;
+        }
+
+        if (myValue >= 4)
+        {
+            bits[2].state = true;
+
+            myValue -= 4;
+        }
+        else
+        {
+            bits[2].state = false;
+        }
+
+        if (myValue >= 2)
+        {
+            bits[1].state = true;
+
+            myValue -= 2;
+        }
+        else
+        {
+            bits[1].state = false;
+        }
+
+        if (myValue >= 1)
+        {
+            bits[0].state = true;
+
+            myValue -= 1;
+        }
+        else
+        {
+            bits[0].state = false;
+        }
+    }
+
+    
 }
+
