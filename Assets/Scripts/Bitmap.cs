a4bf2410d1c5b270c70174cae5a15de20ff92691
D'onell/F4RWAY <donfrank2007@gmail.com>
2025-03-14 13:01:54 +0100

    bitmap

--- Assets/Scripts/Bitmap.cs
+++ Assets/Scripts/Bitmap.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Bitmap : MonoBehaviour
 {
+    [SerializeField] private int[] values = new int[8];
+
+    [SerializeField] private Byte[] bytes = new Byte[8];
     // Start is called before the first frame update
     void Start()
     {
         
+
+        // Update is called once per frame
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < 8; i++)
+        {
+            bytes[i].setValue(values[i]);
+        }
     }
 }
